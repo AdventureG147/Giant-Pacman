0e52de2e7494d438c1d6eb150f4e965391eaeb8c
AdventureG147 <bsbalash99@gmail.com>
2023-05-26 21:07:54 +0300

    Some Changes, part 2

--- src/main/java/net/adventureg147/giantpacman/client/entity/model/GiantPacmanEntityModel.java
+++ src/main/java/net/adventureg147/giantpacman/client/entity/model/GiantPacmanEntityModel.java
@@ -3,7 +3,10 @@ package net.adventureg147.giantpacman.client.entity.model;
 import net.adventureg147.giantpacman.GiantPacman;
 import net.adventureg147.giantpacman.common.entity.GiantPacmanEntity;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class GiantPacmanEntityModel extends AnimatedGeoModel<GiantPacmanEntity> {
 	@Override
@@ -19,5 +22,21 @@ public class GiantPacmanEntityModel extends AnimatedGeoModel<GiantPacmanEntity> 
 	@Override
 	public ResourceLocation getAnimationFileLocation(GiantPacmanEntity animatable) {
 		return new ResourceLocation(GiantPacman.MODID, "animations/giant_pacman.animation.json");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void setLivingAnimations(GiantPacmanEntity entity, Integer uniqueID, @SuppressWarnings("rawtypes") AnimationEvent customPredicate) {
+		super.setLivingAnimations(entity, uniqueID, customPredicate);
+
+		IBone top = this.getAnimationProcessor().getBone("top");
+		IBone bottom = this.getAnimationProcessor().getBone("bottom");
+		EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+		top.setScaleX(6.0f);
+		top.setScaleY(6.0f);
+		top.setScaleZ(6.0f);
+		bottom.setScaleX(6.0f);
+		bottom.setScaleY(6.0f);
+		bottom.setScaleZ(6.0f);
 	}
 }
