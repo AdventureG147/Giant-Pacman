94f6c64902650e69b556904d41eafb027a8d6fbd
Blackout03 <gardner.hayden03@gmail.com>
2023-06-08 17:23:53 +1000

    Some changes, part 9
    - Fixed Pacman looking away from player.
    - Fixed Pacman Animation being weird.
    - Updated Pacman Trophy(/Statue) Model.
    - Updated Pacman Model.
    - Pacman no longer takes fall damage.
    - Pacman trophy now has custom hitbox/collision shape.
    - Updated all the trophy(/statue) textures.

--- src/main/java/net/adventureg147/giantpacman/common/block/TrophyBlock.java
+++ src/main/java/net/adventureg147/giantpacman/common/block/TrophyBlock.java
@@ -6,15 +6,23 @@ import net.minecraft.block.HorizontalBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nullable;
 
 import static net.minecraft.state.properties.BlockStateProperties.HORIZONTAL_FACING;
 
 public class TrophyBlock extends HorizontalBlock {
+	protected static final VoxelShape BASE_1 = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
+	protected static final VoxelShape BASE_2 = Block.box(1.0D, 2.0D, 1.0D, 15.0D, 3.0D, 15.0D);
+	protected static final VoxelShape BASE = VoxelShapes.or(BASE_1, BASE_2);
+	protected static final VoxelShape STAND = Block.box(6.0D, 3.0D, 6.0D, 10.0D, 5.0D, 10.0D);
+	protected static final VoxelShape PACMAN = Block.box(1.0D, 5.0D, 1.0D, 15.0D, 20.0D, 15.0D);
+	protected static final VoxelShape SHAPE = VoxelShapes.or(BASE, STAND, PACMAN);
+
 	public TrophyBlock(Properties builder) {
 		super(builder);
 	}
@@ -30,9 +38,23 @@ public class TrophyBlock extends HorizontalBlock {
 		return this.defaultBlockState().setValue(HORIZONTAL_FACING, context.getHorizontalDirection().getOpposite());
 	}
 
-	@OnlyIn(Dist.CLIENT)
-	public float getShadeBrightness(BlockState pState, IBlockReader pLevel, BlockPos pPos) {
-		return 1.0F;
+	@Override
+	public VoxelShape getShape(BlockState pState, IBlockReader pLevel, BlockPos pPos, ISelectionContext pContext) {
+		return SHAPE;
+	}
+
+	@Override
+	public VoxelShape getCollisionShape(BlockState pState, IBlockReader pLevel, BlockPos pPos, ISelectionContext pContext) {
+		return SHAPE;
+	}
+
+	@Override
+	public VoxelShape getOcclusionShape(BlockState pState, IBlockReader pLevel, BlockPos pPos) {
+		return SHAPE;
+	}
+
+	public boolean useShapeForLightOcclusion(BlockState pState) {
+		return true;
 	}
 
 	public boolean propagatesSkylightDown(BlockState pState, IBlockReader pReader, BlockPos pPos) {
--- src/main/java/net/adventureg147/giantpacman/common/events/EntitySetAttributeEventSubscriber.java
+++ src/main/java/net/adventureg147/giantpacman/common/events/EntitySetAttributeEventSubscriber.java
@@ -2,7 +2,12 @@ package net.adventureg147.giantpacman.common.events;
 
 import net.adventureg147.giantpacman.common.registry.GPEntityTypes;
 import net.adventureg147.giantpacman.common.entity.GiantPacmanEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EntitySetAttributeEventSubscriber {
